dd80ede6399ec98a29488fb3a0a485982d202bc0
sophiedick <sophiefdick@gmail.com>
2015-12-08 20:24:07 +0000

    css altered

--- project1/README.md
+++ project1/README.md
@@ -1,11 +1,18 @@
 General Assembly Project One December 2015
 
-Why?
+**** Why? ****
 This game was designed for a project on a twelve week Web Development Immersive course at General Assembly. The brief was to build a two player Javascript game that maintained a score log and was able to detect a winner on a set of winning parameters. Additionally, the game needed to be deployed online for anyone to access.
 
-Technical Requirements: The game design uses individual HTML, CSS and Javascript files. jQuery has also been used for DOM manipulation.
+Brain Training 2.0 is an educational game for 2 players, designed to test both memory and concentration levels. The player is shown 4 lists each containing 7 items. Each of the 4 lists will populate individually for 10 seconds, during this time the user should try to memorize all 7 items in the list. 
 
-Further deliverables:
+After 40 seconds all the lists will have been shown. A random generator function will then ask the player to recall the 7 items in ONE of the 4 lists. The player who remembers the most items is the winner!
+
+
+***Technical Requirements ****
+The game design uses individual HTML, CSS and Javascript files. jQuery has also been used for DOM manipulation.
+
+*** Further deliverables ***
+
 a. Build out difficulty levels, so user can progress through the game. 
 b. Ability to collect badges as they go along which unlock different levels, hopefully help to cement stickiness. 
 c. Toggle option for single or multi-player.
--- project1/js/index.js
+++ project1/js/index.js
@@ -1,23 +1,20 @@
 $(document).ready(function() {
 
+//SoundManager Set up function
 	soundManager.setup({
 	  url: '/path/to/swf-files/',
 	  flashVersion: 9,
 });
-//Have console all variables logged these all and they work!
+//Global variables
 
-	var List1 = $(".List1");
-	//console.log(List1); 
-
+	var List1 = $(".List1"); 
 	var List1Array = [];
 	List1.each(function() {
 		List1Array.push($(this).text());
 		console.log(List1Array);
 	});
 
-	var List2 = $(".list2");
-	//console.log(List2); 
-
+	var List2 = $(".list2"); 
 	var List2Array = [];
 	List2.each(function() {
 		List2Array.push($(this).text());
@@ -25,8 +22,6 @@ $(document).ready(function() {
 	});
 
 	var List3 = $(".list3");
-	//console.log(List3); 
-
 	var List3Array = [];
 	List3.each(function() {
 		List3Array.push($(this).text());
@@ -34,115 +29,63 @@ $(document).ready(function() {
 	});
 
 	var List4 = $(".list4");
-
 	var List4Array = [];
 	List4.each(function() {
 		List4Array.push($(this).text());
 		console.log(List4Array);
 	});
 
-	//console.log(List1Array); This works it pushes all the text to an array to be compared later. 
-
 	var boyNames = $(".boyNames");
-	// console.log(boyNames);
-
 	var girlNames = $(".girlNames");
-	// console.log(girlNames);
-
 	var countries = $(".Countries");
-	// console.log(countries);
-
 	var gameContainer = $("#gameContainer");
-	// console.log(gameContainer);
-
 	var messages = $("#messages");
-
 	var playerText = $("#playerText");
 	var submitButton = $("#submitButton");
 	var myNewTextElement = $("<input type='text' value=''>");
-
 	var playerWords = [];
 	var wrongGuesses = [];
-
 	var clickCount = 0;
 	var counter = 0;
 	var gameCount = 0;
 	var playerOneTurn = true;
 	var lives = 7;
-
 	var playerOneScoreboard = $("#player1Scoreboard");
 	var playerTwoScoreboard = $("#player2Scoreboard");
+	var playerOne;
+	var playerTwo; 
 
-	//Instructions needed here:
-
-	var playerOne; //= prompt("Player one please enter your name?");
-	// console.log(playerOne);
-	// $("#player1Name").html(playerOne);
-
-	var playerTwo; // = prompt("Player two please enter your name?");
-	// console.log(playerTwo);
-	// $("#player2Name").html(playerTwo);
-
-
-	//This function shows Grocery List then hides it. 
+	//Function below show and hide the list items: Have used .show(), .hide(), setTimeout()
 	function showList1(){
 		$("#groceryList").show();
-		// $("#gameContainer").css('background-image', 'url("http://alabamamaps.ua.edu/contemporarymaps/world/europe/europe2.jpg")');
-
 		List1.show();
 		$(".listContainer").animate({left: "+=850"}, 10000);
-		// $(".listContainer").animate({left: "-=1200"}, 8000);
-		// $(".listContainer").animate({left: "+=1200"}, 7000);
-		// $(".listContainer").animate({left: "-=1200"}, 6000);
-		// $(".listContainer").animate({left: "+=1200"}, 5000);
-		// $(".listContainer").animate({left: "-=1200"}, 4000);
 		setTimeout(function(){
 			$("#groceryList").hide();
 			List1.hide();
-			// $("#gameContainer").hide.css('background-image', 'url("http://alabamamaps.ua.edu/contemporarymaps/world/europe/europe2.jpg")');
 			$(".listContainer").animate({left: "-=850"}, 10000);
-			//function to pop up instructions - tell user to start entering guesses and hit submit
 		}, 8000); 
 	}
-
 
 	function showList2(){
 		$("#boysNames").show();
 		List2.show();
 		$(".listContainer2").animate({left: "+=850"}, 10000);
-		// $(".listContainer2").animate({left: "-=1200"}, 4000);
-		// $(".listContainer2").animate({left: "+=1200"}, 4000);
-		// $(".listContainer2").animate({left: "-=1200"}, 4000);
-		// $(".listContainer2").animate({left: "+=1200"}, 4000);
-		// $(".listContainer2").animate({left: "-=1200"}, 4000);
-		// $(".listContainer2").animate({left: "+=1200"}, 4000);
-		// $(".listContainer2").animate({left: "-=1200"}, 4000);
-
 		setTimeout(function(){
 			$("#boysNames").hide();
 			List2.hide();
 			$(".listContainer2").animate({left: "-=850"}, 10000);
-			//function to pop up instructions - tell user to start entering guesses and hit submit
 		}, 10000); 
 	}
-
 
 	function showList3(){
 		$("#girlsNames").show();
 		List3.show();
 		$(".listContainer3").animate({left: "+=850"}, 10000);
-		// $(".listContainer3").animate({left: "-=1200"}, 2000);
-		// $(".listContainer3").animate({left: "+=1200"}, 2000);
-		// $(".listContainer3").animate({left: "-=1200"}, 2000);
-		// $(".listContainer3").animate({left: "+=1200"}, 2000);
-		// $(".listContainer3").animate({left: "-=1200"}, 2000);
-		// $(".listContainer3").animate({left: "+=1200"}, 2000);
-		// $(".listContainer3").animate({left: "-=1200"}, 2000);
 		setTimeout(function(){
 			$("#girlsNames").hide();
 			List3.hide();
 			$(".listContainer3").animate({left: "-=850"}, 10000);
-			//function to pop up instructions - tell user to start entering guesses and hit submit
 		}, 10000); 
 	}
 
@@ -150,23 +93,14 @@ $(document).ready(function() {
 		$("#europeanCities").show();
 		List4.show();
 		$(".listContainer4").animate({left: "+=850"}, 10000);
-		// $(".listContainer4").animate({left: "-=1200"}, 2000);
-		// $(".listContainer4").animate({left: "+=1200"}, 2000);
-		// $(".listContainer4").animate({left: "-=1200"}, 2000);
-		// $(".listContainer4").animate({left: "+=1200"}, 2000);
-		// $(".listContainer4").animate({left: "-=1200"}, 2000);
-		// $(".listContainer4").animate({left: "+=1200"}, 2000);
-		// $(".listContainer4").animate({left: "-=1200"}, 2000);
 		setTimeout(function(){
 			$("#europeanCities").hide();
 			List4.hide();
 			$(".listContainer4").animate({left: "-=850"}, 10000);
-			//function to pop up instructions - tell user to start entering guesses and hit submit
 		}, 8000); 
 	}
 
-
-
+// These functions randomly generate lists
 	var thisGameAnswerNumber; 	
 	var thisGameAnswerArray;
 
@@ -183,28 +117,28 @@ $(document).ready(function() {
 	}
 
 	function pickAList() {
-    var randomNumber = Math.random();
-    if (randomNumber < 0.24) {
-        return 1;
-    } else if (randomNumber < 0.49) {	    	
-        return 2;
-    } else if (randomNumber < 0.75) {
-        return 3;
-    } else {
-        return 4;
-    } 
+    	var randomNumber = Math.random();
+    	if (randomNumber < 0.24) {
+        	return 1;
+    	} else if (randomNumber < 0.49) {	    	
+        	return 2;
+    	} else if (randomNumber < 0.75) {
+        	return 3;
+    	} else {
+        	return 4;
+    	} 
 	}
 
 	function displayQuestion(number) {
-    if (number ===1) {
+    	if (number ===1) {
     	messages.html("Please enter as many Grocery List items as you can remember");
-    } else if (number === 2) {
+    	} else if (number === 2) {
     	messages.html("Please enter as many Boys Names as you can remember");
-    } else if (number === 3) {
+    	} else if (number === 3) {
     	messages.html("Please enter as many Girls Names as you can remember");
-    } else if (number ===4){
+    	} else if (number ===4){
     	messages.html("Please enter as many European Countries as you can remember");
-    } 
+    	} 
 	}
 
 	submitButton.click(function() {
@@ -213,32 +147,21 @@ $(document).ready(function() {
 
 	function checkWins(number) { 
 		// This function pushes all the players guesses to an array named player words 
-		// COMPARE ONE ELEMENT AT A TIME WITH THE ARRAY: THIS WORKS could be tidied up but mostly working. 
-		// console.log(playerText.val());
-		console.log(thisGameAnswerArray);
-		var gameArray = listGenerator(number);
-		
+		var gameArray = listGenerator(number);	
 		if (($.inArray(playerText.val(), gameArray)) != -1) {	
-			// console.log(thisGameAnswerArray);
 		  messages.html("Correct!!");
 		  messages.css("background-color","#00C853");
 		  correctSound();
-			playerWords.push(playerText.val());
+		  playerWords.push(playerText.val());
 		  var itemToRemove = playerText.val();
 		  gameArray.splice($.inArray(itemToRemove, gameArray),1);
-
 		  playerText.val("");
 			counter++;
 			updateScore();
-			//$("#player1Scoreboard").html(counter);
 			clickCount++;
 			lives--;
 			$("#answerInputs p").html(lives);
-			console.log(lives);
-			console.log(clickCount);
-	    	console.log(counter);
-			console.log(playerWords);
-
+			
 			if (clickCount == 7) {	
 				messages.html("That is the end of your turn, your score is " + counter + "/7!");
 				gameCount++;
@@ -254,7 +177,6 @@ $(document).ready(function() {
 			} else {
 			 		compareScores();
 			}
-			    //compareScores();
 			}
 
 		} else {
@@ -267,8 +189,6 @@ $(document).ready(function() {
 			clickCount++;
 			lives--;
 			$("#answerInputs p").html(lives);
-			// gameCount++;
-			// updateScore();
 			console.log(clickCount);
 
 				if (clickCount == 7) {
@@ -318,26 +238,22 @@ $(document).ready(function() {
 		counter = 0;
 		clickCount = 0;
 		lives =7;
-		// console.log(clickCount);
-		// console.log(counter);
 		playerWords = [];
 		wrongGuesses =[];
 		console.log(playerWords);
-		// if (playerOneTurn != playerOneTurn) {
-		showList1(); // This calls the showListFunction - lists and shown and hidden.  
+		showList1();  
 		setTimeout(showList2, 10000);
 		setTimeout(showList3, 20000);
 		setTimeout(showList4, 30000);
 		setTimeout(function(){
-		// 	console.log(thisGameAnswerNumber);
 		displayQuestion(thisGameAnswerNumber);
-			// checkWins(thisGameAnswerNumber);
 		}, 40000);	
 			
 	}
 
-	//compareScores();	
+	//This function shows the instructions until clicked and also start the game	
 	function seeInstructions(){
+		startMusic();
 		$("#instructions").click(function() {
 			$("#instructions").hide();
 			startGame();
@@ -351,11 +267,11 @@ $(document).ready(function() {
 		console.log(score2);
 
 		if (score1 > score2) {
-			messages.html(playerOne + " you are the winner!");
-			messages.css("background-color","#c5b358");
+			messages.html(playerOne + " you are the winner! Play again?");
+			messages.css("background-color","#ff008d");
 		}else if (score2 > score1) {
-			messages.html(playerTwo + " you are the winner!");
-			messages.css("background-color","#c5b358");
+			messages.html(playerTwo + " you are the winner! Play again?");
+			messages.css("background-color","#ff008d");
 		}else {
 			messages.html("It's a draw! Why not play again?");
 		}
@@ -380,7 +296,11 @@ $(document).ready(function() {
 		}).play();
 	}
 
-
+	function startMusic(){
+		soundManager.createSound({
+		  url: 'sounds/startmusic.mp3'
+		}).play();
+	}
 
 	function startGame(){
 		playerOne = prompt("Player one please enter your name?");
